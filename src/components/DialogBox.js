b0e421bc24877c55bb49244c0c5373fbbb6e3197
BarbuVlad <barb.vlad@gmail.com>
2021-06-22 15:57:12 +0300

    more_backend_integration

--- src/components/DialogBox.js
+++ src/components/DialogBox.js
@@ -4,8 +4,15 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
 import {useEffect, useState} from "react";
 import Button from  '@material-ui/core/Button';
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
 const DialogBox = ({username, open,openHandle}) => {
 
     //const [openAdd, setOpenAdd] = useState(open);openHandle
@@ -13,18 +20,56 @@ const DialogBox = ({username, open,openHandle}) => {
     const [popupTitle, setPopupTitle] = useState(`Pre-invite fail`);
     //step 1: pre-invite
     const [preInviteResult, setPreInviteResult] = useState(false);
+    const [inviteeId, setInviteeId] = useState(null);
     //step 2: actual invite
     const [inviteResult, setInviteResult] = useState(false);
+    const [openSnack, setOpenSnack] = useState(false);
+    const [snackMessage, setSnackMessage] = useState('');
 
     const handleClosePopup = () => {
         openHandle(false);
       };
 
       const handleSendInvite = async () => { 
+          console.log("ID::",inviteeId);
+        try{
+            const send_invite = await fetch('http://192.168.206.129:5000/api/users/invite', {
+                method: 'PUT',
+                headers: {"Content-type": "application/json", "x-auth-token": localStorage.getItem("token")},
+                body: JSON.stringify({"invitee_id":inviteeId})
+            });
+            const data = await send_invite.json();
+            if(data["code"]==0){///< successfull invite
+                setSnackMessage(data["message"]);
+                setInviteResult(true);
+                setOpenSnack(true);
+                //close dialog pop-up
+                openHandle(false);
 
+            } else if(data["code"]==1|| ///< "Invitee id missing"
+                    data["code"]==2||   ///< "Invitation not sent. Error occurred."
+                    data["code"]==3||   ///< "Conversation already exists between this 2 users"
+                    data["code"]==4){   ///< "Server error"
 
-        //close dialog pop-up
-        openHandle(false);
+                        setSnackMessage(data["message"]);
+                        setInviteResult(false);
+                        setOpenSnack(true);
+                        //close dialog pop-up
+                        openHandle(false);
+                
+            } else{ ///<unknown error
+                setSnackMessage("Unknown error occurred...");
+                setInviteResult(false);
+                setOpenSnack(true);
+                //close dialog pop-up
+                openHandle(false);
+                }
+        
+            } catch(err){
+            //close dialog pop-up
+            openHandle(false);
+                alert("Error at fetch \"invite\"...");
+            }
       };
 
       useEffect(()=>{
@@ -38,6 +83,7 @@ const DialogBox = ({username, open,openHandle}) => {
                 });
                 const data = await login_result.json();
                 if(data["code"]==0){
+                    setInviteeId(data["invitee_id"]);
                     setPreInviteResult(true);
                     setPopupTitle(`Send invite to ${username}`);
                     setPopupText(data["message"]);
@@ -105,6 +151,25 @@ const DialogBox = ({username, open,openHandle}) => {
             </div>
             </Dialog>
             {/* ==== -------------------- ===*/}
+
+{/*Snackbar - show result of send invite action */}
+            {inviteResult ? 
+    (
+                  <Snackbar open={openSnack} autoHideDuration={6000} onClose={()=>{setOpenSnack(false)}}>
+                  <Alert onClose={()=>{setOpenSnack(false)}} severity="success">
+                    {snackMessage}
+                  </Alert>
+                </Snackbar>
+    )
+                :
+    (
+                <Snackbar open={openSnack} autoHideDuration={6000} onClose={()=>{setOpenSnack(false)}}>
+                <Alert onClose={()=>{setOpenSnack(false)}} severity="error">
+                {snackMessage}
+                </Alert>
+              </Snackbar>
+    )  
+            }
         </div>
     )
 }
